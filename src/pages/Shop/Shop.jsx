99fd22a99124aa1f1b57c20e0b5c370f0d9eb7db
Yega13 <yeganyansuren13@gmail.com>
2026-05-05 00:34:40 +0400

    cleanup

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -31,35 +31,6 @@ const HatIcon = () => (
     </svg>
 )
 
-const CraneIcon = () => (
-    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
-        <path d="M21 10H3" />
-        <path d="M21 6V4a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v2" />
-        <path d="M5 10v11" />
-        <path d="M19 10v11" />
-        <path d="M9 10v4" />
-        <path d="M15 10v4" />
-        <path d="M12 3v7" />
-    </svg>
-)
-
-const ConeIcon = () => (
-    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
-        <path d="M12 2L4 22h16L12 2z" />
-        <path d="M6 18h12" />
-        <path d="M8 14h8" />
-        <path d="M10 10h4" />
-    </svg>
-)
-
-const WarningIcon = () => (
-    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-        <path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z" />
-        <line x1="12" y1="9" x2="12" y2="13" />
-        <line x1="12" y1="17" x2="12.01" y2="17" />
-    </svg>
-)
-
 export default function Shop() {
     return (
         <div className="shop">
